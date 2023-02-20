cc9581f08aafb416b6a5af7efe371bc57bdd3658
Hadi Firouzi <hadi.frz.17@gmail.com>
2023-02-20 21:09:36 +0330

    added hackerRank problems no. 4&5&6

---
+++ HackerRank/week1/4.Breaking-The-Records/Breaking-The-Records.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+func breakingRecords(scores []int32) []int32 {
+	// Write your code here
+	var Min_s, Max_s, count_min, count_max int32
+	Min_s = scores[0]
+	Max_s = scores[0]
+	for i := 1; i < len(scores); i++ {
+		if scores[i] < Min_s {
+			Min_s = scores[i]
+			count_min = count_min + 1
+		}
+		if scores[i] > Max_s {
+			Max_s = scores[i]
+			count_max = count_max + 1
+		}
+	}
+	var counts []int32
+	counts = append(counts, count_max)
+	counts = append(counts, count_min)
+	return counts
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	scoresTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var scores []int32
+
+	for i := 0; i < int(n); i++ {
+		scoresItemTemp, err := strconv.ParseInt(scoresTemp[i], 10, 64)
+		checkError(err)
+		scoresItem := int32(scoresItemTemp)
+		scores = append(scores, scoresItem)
+	}
+
+	result := breakingRecords(scores)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
